5c70ef8c2b67615e23bed55f340d589596bc994e
VenkataHarikrishna <machavaramvenkataharikrishna@gmail.com>
2024-02-20 00:07:41 +0530

    update sample

--- src/components/sample.js
+++ src/components/sample.js
@@ -4,7 +4,7 @@ const Sample=()=>{
     return (
         <div className='main-div'>
             <h1 className='text-white bg-black text-lg mb-5'>Welcome</h1>
-            <p className='text-red-900 text-xl'>to the coding</p>
+            <p className='text-red-900 text-xl'>to my college</p>
         </div>
     )
 } 

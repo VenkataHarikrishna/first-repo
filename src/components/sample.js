581abaa3bc27355f5faaae0b7098e01a1fc87418
VenkataHarikrishna <machavaramvenkataharikrishna@gmail.com>
2024-02-20 00:01:25 +0530

    remaining files

---
+++ src/components/sample.js
@@ -0,0 +1,12 @@
+import './sample.css'
+
+const Sample=()=>{
+    return (
+        <div className='main-div'>
+            <h1 className='text-white bg-black text-lg mb-5'>Welcome</h1>
+            <p className='text-red-900 text-xl'>to the coding</p>
+        </div>
+    )
+} 
+
+export default Sample;
